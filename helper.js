8e4a39cdf113ec69a8526e3b7a8e8c386cc6e6e3
🐳 <502533466@qq.com>
2023-09-26 23:44:35 +0800

    feat: 完善逻辑

---
+++ helper.js
@@ -0,0 +1,25 @@
+export function formatTargetDir(targetDir) {
+  return targetDir?.trim().replace(/\/+$/g, '');
+}
+
+export function isValidPackageName(projectName) {
+  return /^(?:@[a-z\d\-*~][a-z\d\-*._~]*\/)?[a-z\d\-~][a-z\d\-._~]*$/.test(projectName);
+}
+
+export function copy(src, dest) {
+  const stat = fs.statSync(src);
+  if (stat.isDirectory()) {
+    copyDir(src, dest);
+  } else {
+    fs.copyFileSync(src, dest);
+  }
+}
+
+export function copyDir(srcDir, destDir) {
+  fs.mkdirSync(destDir, { recursive: true });
+  for (const file of fs.readdirSync(srcDir)) {
+    const srcFile = path.resolve(srcDir, file);
+    const destFile = path.resolve(destDir, file);
+    copy(srcFile, destFile);
+  }
+}
--- index.js
+++ index.js
@@ -2,6 +2,21 @@
 
 import run from './run.js';
 
+const currentNodeVersion = process.versions.node;
+const semver = currentNodeVersion.split('.');
+const major = semver[0];
+
+if (major < 18) {
+  console.error(
+    'You are running Node ' +
+      currentNodeVersion +
+      '.\n' +
+      'Create Axum App requires Node 18 or higher. \n' +
+      'Please update your version of Node.',
+  );
+  process.exit(1);
+}
+
 run().catch((e) => {
   console.error(e);
 });
